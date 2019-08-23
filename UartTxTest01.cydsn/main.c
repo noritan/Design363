93a9e0bd0c19964779b29d477270e5770399243e
Nori Tan <github@noritan.org>
2019-08-23 22:21:37 +0900

    Update main.c
    Created statemachine task
    Created packet[] creation logic
    Revised statemachine task dispatcher
    Added comments

--- UartTxTest01.cydsn/main.c
+++ UartTxTest01.cydsn/main.c
@@ -11,53 +11,83 @@
 */
 #include "project.h"
 
+// SR1 status register bit assignment
 #define SR1_DREQ (1)
 #define SR1_SW1  (2)
 
-CYBIT sending = 0;
+// Statemachine declaration
+#define ST_IDLE (0)
+#define ST_SEND (1)
+#define ST_WAIT (2)
 
-uint8 packet[6] = {0x0f, 0x33, 0x55, 0xaa, 0x71, 0xff};
+uint32 state = ST_IDLE;
 
+// Data packet to be sent
+const char phrase[] = "The quick brown fox jumps over the lazy dog. ";
+uint8 packet[8];
+
+// Interrupt handling
 volatile CYBIT int_Sample_flag = 0;
 
 CY_ISR(int_Sample_isr) {
     int_Sample_flag = 1;
 }
 
+// The main-loop
 int main(void)
 {
     uint32 index = 0;
     
     CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    // Initialize the logic analyzer task
     Probe_UART_Start();
     int_Sample_StartEx(int_Sample_isr);
+    
+    // Initialize the statemachine task
+    state = ST_IDLE;
+
+    // Initialize the packet[]
+    // Copy the phrase[] into packet[] as possible
+    for (uint32 k = 0; k < sizeof packet; ) {
+        for (uint32 i = 0; i < sizeof phrase; i++) {
+            packet[k++] = phrase[i];
+            if (k >= sizeof packet) break;
+        }
+    }
 
     for(;;)
     {
-        /* Place your application code here. */
-        if (SR1_Read() & SR1_SW1) {
-            // SW1 released
-            if (sending) {
-                if (index >= sizeof packet) {
-                    sending = 0;
+        // Statemachine dispatcher
+        switch (state) {
+            case ST_IDLE:
+                // Wait for the SW1 pushed
+                if (!(SR1_Read() & SR1_SW1)) {
+                    // SW1 pushed
                     index = 0;
+                    state = ST_SEND;
                 }
-            }
-        } else {
-            // SW1 pushed
-            if (!sending) {
-                sending = 1;
-            }
-        }
-        if (sending) {
-            if (index < sizeof packet) {
-                if (SR1_Read() & SR1_DREQ) {
+                break;
+            case ST_SEND:
+                // Send a packet until exhausted
+                if ((SR1_Read() & SR1_DREQ)) {
                     UartTx_WriteValue(packet[index++]);
                 }
-            }
+                if (index >= sizeof packet) {
+                    state = ST_WAIT;
+                }
+                break;
+            case ST_WAIT:
+                // Wait for the SW1 released
+                if (SR1_Read() & SR1_SW1) {
+                    state = ST_IDLE;
+                }
+                break;
+            default:
+                state = ST_IDLE;
         }
+        
+        // Logic analyzer dispatcher
         if (int_Sample_flag) {
             int_Sample_flag = 0;
             Probe_UART_PutChar(Probe_Read());
